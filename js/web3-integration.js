40aad411b38599280b5cb14a300138947e1a1df8
Mosano <a894612005@gmail.com>
2022-05-06 11:01:01 +0800

    Update web3-integration.js

--- js/web3-integration.js
+++ js/web3-integration.js
@@ -140,7 +140,6 @@ async function get_wallet_info_web3() {
 
             	// Discount Wallet
             	onDisconnect();
-                location.href ="https://mintverse.world/mint/"
                 return;
             }
 
@@ -160,7 +159,6 @@ async function get_wallet_info_web3() {
             if(maxMint==0){
                 $('.popset').openPop({message:'您已經超過鑄造上限的數量了'});
                 onDisconnect();
-                location.href ="https://mintverse.world/mint/";
             }
 			$('.inputWrap img').on('click', function(event) {
 				event.preventDefault();
@@ -208,7 +206,6 @@ async function mint_whitelist_word(wallet_info) {
     if (mint_num > wallet_info.mint_limit - wallet_info.claimed_Number) {
         $('.popset').openPop({ message: '白名單鑄造數量超過上限', type: 'failed' });
         onDisconnect();
-        location.href ="https://mintverse.world/mint/";
     }
     // check qualification on contract
     const verification = await WordContract.methods.verify(wallet_info.mint_limit,wallet_info.signedSignature).call({from:connectedAddress[0]});
@@ -216,7 +213,6 @@ async function mint_whitelist_word(wallet_info) {
     if(!verification){
         $('.popset').openPop({ message: '白名單資格有問題，請聯繫Discord或FB/IG。', type: 'failed' });
         onDisconnect();
-        location.href ="https://mintverse.world/mint/";
     }
 
     // Execute Minting
